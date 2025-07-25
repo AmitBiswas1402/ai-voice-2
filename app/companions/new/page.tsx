c9412fa8afbe6abcea084731dbb7d6d3b9075494
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-07-25 23:49:03 +0530

    Add form components and validation schema for new companions feature

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
 const NewCompanions = () => {
   return (
-    <div>NewCompanions</div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion</h1>
+      </article>
+    </main>
   )
 }
 export default NewCompanions
