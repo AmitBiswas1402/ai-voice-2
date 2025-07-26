72898d0f6e4eed4b9880c8925bb075264c6c70e5
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-07-26 19:22:49 +0530

    chore: update dependencies to include @clerk/nextjs and @radix-ui/react-select

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,18 +1,14 @@
-"use client"
-
-import { z } from "zod"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import CompanionForm from "@/components/CompanionForm";
 
 const NewCompanions = () => {
   return (
-    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion</h1>
+
+        <CompanionForm />
       </article>
     </main>
-  )
-}
-export default NewCompanions
+  );
+};
+export default NewCompanions;
