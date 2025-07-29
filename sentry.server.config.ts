3da60f1182c0a1705c43018c3f53ec2c405cff00
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-07-29 15:43:08 +0530

    Clean Git tracking by removing build and dependency artifacts

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://538cc623355d6e9482b995568856a1d3@o4509201155358720.ingest.us.sentry.io/4509750799761408",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
